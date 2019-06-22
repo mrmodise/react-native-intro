f658b05c48d9293531ee070354cd1e853f846c40
mrmodise <modisemorebodi@gmail.com>
2019-06-22 20:04:22 +0200

    refactor: add greeting component

---
+++ Greeting.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+import {Text, View} from 'react-native';
+
+class Greeting extends Component {
+    render() {
+        return (
+            <View style={{alignItems: "center"}}>
+                <Text>Hello {this.props.name}!</Text>
+            </View>
+        )
+    }
+}
+
+export default class LotsOfGreetings extends Component {
+    render() {
+        return (
+            <View style={{alignItems: "center"}}>
+               <Greeting name={'Dumela'} />
+               <Greeting name={'Molweni'} />
+               <Greeting name={'Nda'} />
+            </View>
+        );
+    }
+
+}
